c3e3efa10d8ecb50ec4481e7b9353b8935a0708e
Meriem <mekrarbech12@gmail.com>
2023-10-06 10:58:03 -0400

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form method="post" action="result.php">
+        Mot De Passe :
+        <input type="text" name="fname">
+        <input type="submit">
+    </form>
+</body>
+</html>
